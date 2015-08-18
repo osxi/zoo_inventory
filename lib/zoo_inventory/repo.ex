b2d734b3ec55d56d7cd6ef3c84cb5812962555b3
osxi <osxi@users.noreply.github.com>
2015-08-18 00:36:19 -0500

    remove unnecessary repo config

--- lib/zoo_inventory/repo.ex
+++ lib/zoo_inventory/repo.ex
@@ -1,11 +1,3 @@
 defmodule ZooInventory.Repo do
   use Ecto.Repo, otp_app: :zoo_inventory
-
-  def conf do
-    parse_url "ecto://localhost/zoo_inventory_dev"
-  end
-
-  def priv do
-    app_dir(:zoo_inventory, "priv/repo")
-  end
 end
